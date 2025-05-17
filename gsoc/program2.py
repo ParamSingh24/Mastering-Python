27fd553aa8937151d6b9848253919d321226ce15
PARAM SINGH <techhero724@gmail.com>
2025-05-17 23:41:36 +0530

    updating

--- gsoc/program2.py
+++ gsoc/program2.py
@@ -1,5 +1,13 @@
-a = input("number:")
-x = "9"
-b = x
-print(a+b)
- 
+#TO calculate the gcd of two number 
+def gcd(a,b):
+    while b != 0:
+        a , b = b , a%b
+    return a 
+n = int(input("enter number:"))
+b = int(input("enter number:"))
+g = gcd(n,b)
+print(f"the gcd of two numbers are {g}")
+
+
+
+
